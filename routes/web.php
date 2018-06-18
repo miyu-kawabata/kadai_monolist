9b305be43eb32433d96d0bfbe2f19e6d27340fc1
EC2 Default User <miyu.kawabata@rakuten.com>
2018-06-18 03:06:50 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
 Route::get('/', 'WelcomeController@index');
-});
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
